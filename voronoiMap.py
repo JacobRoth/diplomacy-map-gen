f695347ab323637d45bb1f140edba29813da01f4
Daniel <daniel.sonner@gmail.com>
2015-04-11 02:13:43 -0700

    misunderstood sorted. updated

--- voronoiMap.py
+++ voronoiMap.py
@@ -15,9 +15,7 @@ def main():
    # make up random data points
    np.random.seed(seedNum)
    points = np.random.rand(NUM_COUNTRIES, 2)
-   
-   # sorting doesn't seem to be working
-   sorted(points,key=lambda l:math.sqrt(l[0]**2 + l[1]**2), reverse=False)
+   points = sorted(points,key=lambda l:math.sqrt(l[0]**2 + l[1]**2), reverse=False)
 
    # compute Voronoi tesselation
    vor = Voronoi(points)
